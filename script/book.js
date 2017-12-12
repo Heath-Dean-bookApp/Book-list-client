a2162c9074828703164b6e8b5a766dba5fd7a584
heath smith <heathsmith73@gmail.com>
2017-12-12 13:39:23 -0800

    testing

--- script/book.js
+++ script/book.js
@@ -1,30 +1,31 @@
 
-var __API_URL__ = 'http:localhost:3000';
+var __API_URL__ = 'http://localhost:3000/api/v1/books';
 console.log('this is a test of book.js');
 
 function Book(bookDataObj) {
 
-  Object.keys(bookDataObj).forEach(key => this[key] = bookDataObj[key]);
+  Object.keys(bookDataObj).forEach(key => {this[key] = bookDataObj[key];
+  }, this);
 }
 
 Book.all = [];
 
-// Book.prototype.toHtml = function() {
-//   var template = Handlebars.compile($('#book-list-template').text());
-//
-//   return template(this);
-// };
+Book.prototype.toHtml = function() {
+  var template = Handlebars.compile($('#book-list-template').text());
 
-Book.loadAll = bookData => {
-  // rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
-
-  Book.all = bookData.map(bookObject => new Book(bookObject));
+  return template(this);
 };
 
-Book.fetchAll = () => {
-  $.get(`${__API_URL__}/api/v1/books`)
-    .then(
-      Book.loadAll
-      // callback();
-    )
+Book.loadAll = function(rows) {
+  rows.sort((a,b) => a.title - b.title);
+  rows.map(row => Book.all.push(new Book(row)));
+  console.log('load all book.all', Book.all);
+};
+
+Book.fetchAll = function (callback) {
+  $.get(__API_URL__)
+    .then(data => {
+      Book.loadAll(data)
+      if (callback) callback();
+    })
 };
