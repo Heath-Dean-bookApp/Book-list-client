1ce389c5e0c1b1b212878b5d13abd35ff3887d26
heath smith <heathsmith73@gmail.com>
2017-12-13 13:53:52 -0800

    everthing is working on the main page

--- script/views/routes.js
+++ script/views/routes.js
@@ -3,6 +3,6 @@
 
 page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/:book_id', ctx => app.bookView.initSingleBookPage);
-page('/books/new', ctx => app.bookView.initNewBookPage);
+page('/books/new', ctx => app.bookView.initNewBookPage(ctx));
 
 page();
