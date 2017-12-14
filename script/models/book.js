1113dc4518d0603b354ca6608cc4ca8c01d79a51
heath smith <heathsmith73@gmail.com>
2017-12-14 10:30:37 -0800

    test

--- script/models/book.js
+++ script/models/book.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// REMOVE THIS 
-
 var __API_URL__ = 'http://localhost:3000';// local use only
 // var __API_URL__ = 'https://hs-dm-booklist.herokuapp.com';// for deployed testing
 var app = app || {};
